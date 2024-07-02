fca293e717ad75fbbffa6e349337d91a2d30e4e1
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-07-02 03:20:38 +0300

    Add save and find functions

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Give password as an argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://admin:${password}@cluster0.bl5nd0x.mongodb.net/fso_part3?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
