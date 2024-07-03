fe20758463931c8bffb66473e3d836fb67d6eb81
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-07-03 04:30:23 +0300

    Update for curriculum

--- models/note.js
+++ models/note.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 mongoose.set("strictQuery", false);
 
 const url = process.env.MONGODB_URI;
 
 console.log("connecting to", url);
-
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to mongoDB");
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to mongodb", error.message);
+    console.log("error connecting to MongoDB:", error.message);
   });
 
 const noteSchema = new mongoose.Schema({
